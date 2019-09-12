2fe598579aa5608ace1f9d59bf29842d0d9393ba
Hendra <pmghendra@gmail.com>
2019-09-12 11:01:53 +0700

    [hendra] - part 3 sampai end

--- myapp/app/Http/Controllers/StudentsController.php
+++ myapp/app/Http/Controllers/StudentsController.php
@@ -25,7 +25,7 @@ class StudentsController extends Controller
      */
     public function create()
     {
-        //
+        return view ('students.create');
     }
 
     /**
@@ -36,7 +36,35 @@ class StudentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+    //    $student = new Student;
+    //    $student->nama = $request->nama;
+    //    $student->nrp = $request->nrp;
+    //    $student->email = $request->email;
+    //    $student->jurusan = $request->jurusan;
+
+    //    $student ->save();
+
+    // Student::create([
+    //     'nama' => $request->nama,
+    //     'nrp' => $request->nrp,
+    //     'email' => $request->email,
+    //     'jurusan' => $request->jurusan
+
+    // ]);
+
+
+
+    $request->validate([
+        'nama'=>'required',
+        'nrp'=>'required|size:9',
+        'email'=>'required',
+        'jurusan'=>'required'
+    ]);
+
+    Student::create($request->all());
+
+       return redirect('/students')->with('status','Data Mahasiswa Berhasil Ditambahkan!');
+
     }
 
     /**
@@ -47,7 +75,7 @@ class StudentsController extends Controller
      */
     public function show(Student $student)
     {
-        //
+        return view('students.show', compact('student'));
     }
 
     /**
